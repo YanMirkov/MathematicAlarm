a4d57ef64c66ac1439648a548f3e917e9aac9aae
Ivan Maymeskul <ivan.maymeskul@gmail.com>
2015-02-11 00:22:21 +0200

    Initial commit

---
+++ app/src/main/java/com/maymeskul/mathematicalarm/AlarmService.java
@@ -0,0 +1,16 @@
+package com.maymeskul.mathematicalarm;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Ян on 2/10/2015.
+ */
+public class AlarmService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/maymeskul/mathematicalarm/AlarmsActivity.java
@@ -0,0 +1,15 @@
+package com.maymeskul.mathematicalarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ян on 2/10/2015.
+ */
+public class AlarmsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarms_layout);
+    }
+}
