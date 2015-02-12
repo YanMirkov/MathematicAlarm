e4da4b3a8814153379043191aeb5d1ac26b7f199
Alexander Tselkovsky <a.tselkovsky@gmail.com>
2015-02-12 15:27:06 +0200

    Customized add_alarm.xml

--- app/src/main/java/com/maymeskul/mathematicalarm/AddAlarmActivity.java
+++ app/src/main/java/com/maymeskul/mathematicalarm/AddAlarmActivity.java
@@ -2,14 +2,16 @@ package com.maymeskul.mathematicalarm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by Ян on 2/10/2015.
  */
-public class AddAlarmActivity extends Activity {
+public class AddAlarmActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_alarm);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/maymeskul/mathematicalarm/AlarmsActivity.java
+++ app/src/main/java/com/maymeskul/mathematicalarm/AlarmsActivity.java
@@ -3,12 +3,13 @@ package com.maymeskul.mathematicalarm;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 
 /**
  * Created by Ян on 2/10/2015.
  */
-public class AlarmsActivity extends Activity {
+public class AlarmsActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
