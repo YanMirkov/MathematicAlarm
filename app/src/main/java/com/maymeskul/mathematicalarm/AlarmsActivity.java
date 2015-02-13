bd7c16af3b2806e980a53ae4f73e492f74065629
Alexander Tselkovsky <a.tselkovsky@gmail.com>
2015-02-13 11:36:45 +0200

    Добавил комментраии

--- app/src/main/java/com/maymeskul/mathematicalarm/AlarmsActivity.java
+++ app/src/main/java/com/maymeskul/mathematicalarm/AlarmsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Layout;
 import android.view.View;
 
 /**
@@ -14,6 +15,8 @@ public class AlarmsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+       View v = new View(this);
+       v.getSolidColor();
     }
 
     @Override
